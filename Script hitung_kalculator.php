b9f79afd017b9785e169e256c1467766efcea42c
IrsyadMuhammad <irsyadfrozen@gmail.com>
2016-03-19 14:45:06 +0700

    Create Script hitung_kalculator.php

---
+++ Script hitung_kalculator.php
@@ -0,0 +1,23 @@
+<?php
+    $bil1 = $_POST['bil1'];
+    $bil2 = $_POST['bil2'];
+    
+	if ($tambah = $_POST['tambah'])
+    {
+		$hasil = $bil1 + $bil2 ;
+    }
+    if ($kurang = $_POST['kurang'])
+    {
+		$hasil = $bil1 - $bil2;
+    }
+	if ($kali = $_POST['kali'])
+    {
+		$hasil = $bil1 * $bil2 ;
+	}
+    if ($bagi = $_POST['bagi'])
+    {
+		$hasil = $bil1 / $bil2 ;
+    }
+  
+ header ("location:form_kalkulator.php?bil3=$hasil");
+ ?>
